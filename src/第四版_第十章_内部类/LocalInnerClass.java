68701218010a345b0261f5b9c3007d986fc6a7fb
zhouyf <376298333@qq.com>
2018-03-14 16:54:41 +0800

    在外部类的方法中定义内部类，非匿名内部类

---
+++ src/第四版_第十章_内部类/LocalInnerClass.java
@@ -0,0 +1,57 @@
+package 第四版_第十章_内部类;
+
+/**
+ * Function	: LocalInnerClass.java
+ * Author	: zhouyf
+ * Date		: 2018年3月14日
+ * Version	: 1.0 
+ * Desc		: 局部内部类, 即在外部类的方法中定义内部类  <vs.> 匿名内部类，外部类方法返回时当场定义
+ * History	:
+ */
+
+import static util.Print.*;
+
+interface Counter {
+	int next();
+}
+
+public class LocalInnerClass {
+
+	/* 【共享的变量】 局内 & 匿内*/
+	private int count = 0;
+	
+	/* 局部内部类 : 外部类方法中使用内部类 */
+	Counter getCounter(final String name) {
+		
+		/* 局部内部类：  LocalCounter 实现了 Counter 接口*/
+		class LocalCounter implements Counter {
+			public LocalCounter()	{ print("LocalCounter()"); }
+			public int next()		{ printnb(name); return count++; }
+		}
+		return new LocalCounter();
+	}
+	
+	/* 匿名内部类 : 外部类方法中使用匿名内部类 */
+	Counter getCounter2(final String name) {
+		return new Counter() {
+			{ print("Counter()"); }	//匿名内部类构造器
+			public int next()	{ printnb(name); return count++; }
+		};
+	}
+	
+	public static void main(String[] args) {
+		LocalInnerClass lic = new LocalInnerClass();
+		/* 分别顶一个局部内部类 & 一个匿名内部类*/
+		Counter
+			c1 = lic.getCounter("Local inner : "),
+			c2 = lic.getCounter2("Anony inner : ");
+
+		for(int i = 0; i < 5; i++)
+			print(c1.next());
+		
+		for(int i = 0; i < 5; i++)
+			print(c2.next());
+
+	}
+
+}
