775c07a3688df567f6b58ea0f04aac390c125c51
zhouyf <376298333@qq.com>
2018-03-21 15:05:33 +0800

    第四版_第十一章_持有对象_辅助类typeinfo

--- src/第四版_第十一章_持有对象_辅助类typeinfo/Pets.java
+++ src/第四版_第十一章_持有对象_辅助类typeinfo/Pets.java
@@ -4,15 +4,19 @@ package 第四版_第十一章_持有对象_辅助类typeinfo;
 import java.util.*;
 
 public class Pets {
-  public static final PetCreator creator =
-    new LiteralPetCreator();
-  public static Pet randomPet() {
-    return creator.randomPet();
-  }
-  public static Pet[] createArray(int size) {
-    return creator.createArray(size);
-  }
-  public static ArrayList<Pet> arrayList(int size) {
-    return creator.arrayList(size);
-  }
+	
+	public static final PetCreator creator =
+			new LiteralPetCreator();
+  
+	public static Pet randomPet() {
+	  return creator.randomPet();
+	}
+	
+	public static Pet[] createArray(int size) {
+	  return creator.createArray(size);
+	}
+	
+	public static ArrayList<Pet> arrayList(int size) {
+		return creator.arrayList(size);
+	}
 } ///:~
