5ab0853432f3fb6cd7f4707afb6cb42119bdac80
zhouyf <376298333@qq.com>
2018-04-26 15:24:38 +0800

    异常处理 :)

---
+++ src/第四版_第十二章_通过异常处理错误/package-info.java
@@ -0,0 +1,16 @@
+
+/**
+ * Function	: package-info.java
+ * Author	: zhouyf
+ * Date		: 2018年4月26日 
+ * Version	: 1.0 
+ * Desc		: 十二章 - 通过异常处理错误
+ * History	:
+ */
+package 第四版_第十二章_通过异常处理错误;
+
+/*
+ * 
+ * (1) InteritingExceptions.java	: 自定义异常 (继承自某个系统异常)
+ * 
+ */
