10e3fa03f6ecaee892d19f40ccac2b2824d7f131
zhouyf <376298333@qq.com>
2018-05-08 15:23:25 +0800

    java 重载了 String 对象的操作符 '+'

---
+++ src/第四版_第十三章_字符串/Concatenation.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package 第四版_第十三章_字符串;
+
+/**
+ * Function	: Concatenation.java
+ * Author	: zhouyf
+ * Date		: 2018年5月8日 
+ * Version	: 1.0 
+ * Desc		: java 重载了 String 对象的操作符 '+' 
+ * History	:
+ */
+public class Concatenation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mango = "MANGO";
+		String s = "abc" + mango + "def" + "47";
+		System.out.println("s=" + s);
+	}
+
+}
--- src/第四版_第十三章_字符串/package-info.java
+++ src/第四版_第十三章_字符串/package-info.java
@@ -12,5 +12,7 @@
 package 第四版_第十三章_字符串;
 
 /*
- * (1) Immutable.java	: String对象不变性 : 使用 String 只会 copy 一份引用，不是通过地址传值 => 原 String不变
+ * (1) Immutable.java		: String对象不变性 : 使用 String 只会 copy 一份引用，不是通过地址传值 => 原 String不变
+ * (2) Concatenation.java	: java 重载了 String 对象的操作符 '+' 
+ * 
  */
