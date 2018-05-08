7030e3eba446ba6d6df9801cb876f2c79ba15a74
zhouyf <376298333@qq.com>
2018-05-08 14:44:40 +0800

    使用 Exception 捕获所有异常

---
+++ src/第四版_第十二章_通过异常处理错误/ExceptionMethods.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package 第四版_第十二章_通过异常处理错误;
+
+/**
+ * Function	: ExceptionMethods.java
+ * Author	: zhouyf
+ * Date		: 2018年5月7日 
+ * Version	: 1.0 
+ * Desc		: 使用 Exception 捕获所有异常
+ * History	:
+ */
+
+import static util.Print.*;
+
+public class ExceptionMethods {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			throw new Exception("My Exception");
+		} catch (Exception e) {
+			print("Caught Exception.");
+			print("getMessage() : " + e.getMessage());
+			print("getLocalizedMessage() : " + e.getLocalizedMessage());
+			print("toString() : " + e.toString());
+			print("printStackTrace() >>> ");
+			e.printStackTrace(System.out);
+		}
+	}
+
+}
--- src/第四版_第十二章_通过异常处理错误/package-info.java
+++ src/第四版_第十二章_通过异常处理错误/package-info.java
@@ -15,5 +15,6 @@ package 第四版_第十二章_通过异常处理错误;
  * (3) LoggingExceptions.java		: 异常与日志记录 (使用 System.err 生成错误信息)
  * (4) LoggingExceptions2.java		: 在异常处理程序中生成日志消息 ( 使用 日志logException 生成错误信息 )
  * (5) ExtraFeatures.java			: 进一步定义异常（增加构造器和成员）
+ * (6) ExceptionMethods.java		: 使用 Exception 捕获所有异常
  * 
  */
