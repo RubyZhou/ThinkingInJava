842687c520973f9e8860046e06da0611a92e37c8
zhouyf <376298333@qq.com>
2018-05-08 15:41:00 +0800

    使用 String 运算符 += 【vs】 使用 StringBuilder 对象的 append : StringBuilder 更简洁，+= 则依赖编译器优化

--- src/第四版_第十三章_字符串/package-info.java
+++ src/第四版_第十三章_字符串/package-info.java
@@ -12,8 +12,8 @@
 package 第四版_第十三章_字符串;
 
 /*
- * (1) Immutable.java		: String对象不变性 : 使用 String 只会 copy 一份引用，不是通过地址传值 => 原 String不变
- * (2) Concatenation.java	: java 重载了 String 对象的操作符 '+' 
- * (3) WhiterStringBuilder.java	: 
+ * (1) Immutable.java			: String对象不变性 : 使用 String 只会 copy 一份引用，不是通过地址传值 => 原 String不变
+ * (2) Concatenation.java		: java 重载了 String 对象的操作符 '+' 
+ * (3) WhiterStringBuilder.java	: 使用 String 运算符 += 【vs】 使用 StringBuilder 对象的 append : StringBuilder 更简洁，+= 则依赖编译器优化
  * 
  */
