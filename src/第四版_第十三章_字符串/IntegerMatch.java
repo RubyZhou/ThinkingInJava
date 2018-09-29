a6c7db7169c2d8c56b4821e25a0839a20ffc7271
zhouyf <376298333@qq.com>
2018-09-29 18:55:06 +0800

    使用正则表达式进行匹配 : (1)

---
+++ src/第四版_第十三章_字符串/IntegerMatch.java
@@ -0,0 +1,14 @@
+package 第四版_第十三章_字符串;
+
+
+/**
+ * 使用正则表达式进行匹配 : (1)
+ */
+public class IntegerMatch {
+    public static void main(String[] args) {
+        System.out.println("-1234".matches("-?\\d+"));
+        System.out.println("5678".matches("-?\\d+"));
+        System.out.println("+911".matches("-?\\d+"));
+        System.out.println("+911".matches("(-|\\+)?\\d+"));
+    }
+}
