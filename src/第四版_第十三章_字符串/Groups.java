7cc5f13ae11995802942dc0985698c768dce6787
zhouyf <376298333@qq.com>
2018-10-08 14:51:59 +0800

    正则表达式 : 分组

--- src/第四版_第十三章_字符串/Groups.java
+++ src/第四版_第十三章_字符串/Groups.java
@@ -15,7 +15,7 @@ import static 第四版_源码_util.Print.printnb;
  */
 public class Groups {
 
-    static public final String POEM =
+    static private final String POEM =
         "Twas brillig, and the slithy toves\n" +
         "Did gyre and gimble in the wabe.\n" +
         "All mimsy were the borogoves,\n" +
@@ -29,6 +29,7 @@ public class Groups {
         Matcher m = Pattern.compile("(?m)(\\S+)\\s+((\\S+)\\s+(\\S+))$").matcher(POEM);
 
         while (m.find()) {
+
             for (int j = 0; j <= m.groupCount(); j++) {
                 printnb("[" + m.group() + "]");
                 printnb("[" + j + "] : [" + m.group(j) + "]");
