7499b2baa69403c5c68f9f456b9163f8fde8fe10
zhouyf <376298333@qq.com>
2018-10-17 10:33:06 +0800

    辅助类 : 生成一个已填充的 Pet 数组 / 链表

---
+++ src/第四版_第十四章_类型信息/pets/PetCreator.java
@@ -0,0 +1,46 @@
+package 第四版_第十四章_类型信息.pets;
+
+import java.util.*;
+
+/**
+ *  辅助类 : 生成一个已填充的 Pet 数组 / 链表
+ */
+public abstract class PetCreator {
+
+    private Random rand = new Random(47);
+
+    // 抽象方法 : 这里不指定返回值 List 的具体类型
+    public abstract List<Class<? extends Pet>> types();
+
+    // TODO : 随机生成一个 Pet
+    public Pet randomPet() {
+        int n = rand.nextInt(types().size());
+
+        try {
+            return types().get(n).newInstance();
+        }
+        catch (InstantiationException e) { throw new RuntimeException(e); }
+        catch (IllegalAccessException e) { throw new RuntimeException(e); }
+    }
+
+    // TODO : 创建一个给定大小的 Pet[] 数组，并随机填充
+    public Pet[] createArray(int size) {
+        Pet[] result = new Pet[size];
+
+        for (int i = 0; i < size; i++) {
+            result[i] = randomPet();
+        }
+
+        return result;
+    }
+
+    // TODO : 创建一个 Pet 链表
+    public ArrayList<Pet> arrayList(int size) {
+        ArrayList<Pet> result = new ArrayList<>();
+        Collections.addAll(result, createArray(size));
+        return result;
+
+    }
+
+}
+
