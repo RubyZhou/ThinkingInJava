e2fe529cae7c90c801439c035224ceb593629ef8
zhouyf <376298333@qq.com>
2018-11-05 16:07:07 +0800

    调用 PetCount (Pet 计数器) 来测试 LiteralPetCreator

---
+++ src/第四版_第十四章_类型信息/pets/PetCount2.java
@@ -0,0 +1,10 @@
+package 第四版_第十四章_类型信息.pets;
+
+/**
+ *  Function : 调用 PetCount (Pet 计数器) 来测试 LiteralPetCreator
+ */
+public class PetCount2 {
+    public static void main(String[] args) {
+        PetCount.countPets(Pets.creator);
+    }
+}
