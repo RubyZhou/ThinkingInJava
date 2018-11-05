d13f0913da32bdcab8cd17c9621380dab3b09587
zhouyf <376298333@qq.com>
2018-11-05 15:39:01 +0800

    使用了 LiteralPetCreator 实现的外观类

--- src/第四版_第十四章_类型信息/pets/Pets.java
+++ src/第四版_第十四章_类型信息/pets/Pets.java
@@ -1,12 +1,16 @@
 package 第四版_第十四章_类型信息.pets;
 
+import java.util.ArrayList;
+
+/**
+ *  Function : 使用了 LiteralPetCreator 实现的外观类
+ */
 
 public class Pets {
 
     public static final PetCreator creator = new LiteralPetCreator();
 
-    public static Pet randomPet() {
-        return creator.randomPet();
-    }
-
+    public static Pet               randomPet()             {   return creator.randomPet();         }
+    public static Pet[]             createArray(int size)   {   return creator.createArray(size);   }
+    public static ArrayList<Pet>    arrayList(int size)     {   return creator.arrayList(size);     }
 }
