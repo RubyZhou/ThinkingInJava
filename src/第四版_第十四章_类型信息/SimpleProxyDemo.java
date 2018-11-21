06969acf45ed31ee3a20295b00f06c50d0e22557
zhouyf <376298333@qq.com>
2018-11-21 19:19:21 +0800

    一个简单的代理模式实现

--- src/第四版_第十四章_类型信息/SimpleProxyDemo.java
+++ src/第四版_第十四章_类型信息/SimpleProxyDemo.java
@@ -4,11 +4,18 @@ package 第四版_第十四章_类型信息;
 /**
  *  一个简单的代理模式实现
  */
+
+/**
+ *  实际类 和 代理类 共同实现的接口
+ */
 interface Interface {
     void    doSomething();
     void    somethingElse(String arg);
 }
 
+/**
+ * 实际类 : 真实完成接口实现
+ */
 class RealObject implements Interface {
     @Override
     public void doSomething() {
@@ -21,6 +28,9 @@ class RealObject implements Interface {
     }
 }
 
+/**
+ *  代理类 ： 将实际类以接口的方式传入, 然后在自己实现接口的地方通过调用实际类的方式完成相应功能( 同时追加一部分额外的功能 )
+ */
 class SimpleProxy implements Interface {
     private Interface   proxied;
 
@@ -48,10 +58,11 @@ public class SimpleProxyDemo {
     }
 
     public static void main(String[] args) {
-        System.out.println("---------------------------------");
-        consumer(new RealObject());                     // 直接调用
-        System.out.println("---------------------------------");
-        consumer(new SimpleProxy(new RealObject()));    // 通过代理调用
+        System.out.println("-------------- 直接调用 -------------------");
+        consumer(new RealObject());
+
+        System.out.println("-------------- 通过代理调用 -------------------");
+        consumer(new SimpleProxy(new RealObject()));
     }
 
 }
