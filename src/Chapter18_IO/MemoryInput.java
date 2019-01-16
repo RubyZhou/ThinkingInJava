57c5346e0477bb0f1163a88eb5a02f9b9ce938f8
zhouyf <376298333@qq.com>
2019-01-16 16:50:22 +0800

    文件到内存后使用()

---
+++ src/Chapter18_IO/MemoryInput.java
@@ -0,0 +1,20 @@
+package Chapter18_IO;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ *  文件到内存后使用()
+ */
+public class MemoryInput {
+
+    public static void main(String[] args) throws IOException {
+
+        StringReader in = new StringReader(BufferedInputFile.read("src\\Chapter18_IO\\MemoryInput.java"));
+        int c;
+
+        while ((c = in.read()) != -1) {
+            System.out.print((char)c);  // 必须强制转换 : read() 会以int类型返回(ASCII码)
+        }
+    }
+}
