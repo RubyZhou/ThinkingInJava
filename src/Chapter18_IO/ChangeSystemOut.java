4b428392ec95ac837f8b7b9cb0b64566f75a27b2
zhouyf <376298333@qq.com>
2019-01-21 15:52:30 +0800

    Turn System.out into a PrintWriter.

---
+++ src/Chapter18_IO/ChangeSystemOut.java
@@ -0,0 +1,14 @@
+package Chapter18_IO;
+
+
+import java.io.PrintWriter;
+
+/**
+ *  Turn System.out into a PrintWriter.
+ */
+public class ChangeSystemOut {
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out, true);
+        out.println("Hello, world!");
+    }
+}
