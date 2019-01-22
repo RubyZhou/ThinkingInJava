d440884867604c1bc1bf97c4963ba917d222d81c
zhouyf <376298333@qq.com>
2019-01-22 18:26:11 +0800

    使用 channels 和 buffers 复制文件

---
+++ src/Chapter18_IO/ChannelCopy.java
@@ -0,0 +1,36 @@
+package Chapter18_IO;
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ *  使用 channels 和 buffers 复制文件
+ */
+public class ChannelCopy {
+    private static final int SIZE = 1024;
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("arguments : sourcefile destfile");
+            System.exit(1);
+        }
+
+        // 定义输入输出通道
+        FileChannel
+                in  = new FileInputStream(args[0]).getChannel(),
+                out = new FileOutputStream(args[1]).getChannel();
+
+        // 定义缓冲区
+        ByteBuffer buffer = ByteBuffer.allocate(SIZE);
+
+        while (in.read(buffer) != -1) {
+            buffer.flip();
+            out.write(buffer);
+            buffer.clear();
+        }
+    }
+}
+
