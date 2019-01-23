56243c927f6a416396b04f369f18fead61ebd8e8
zhouyf <376298333@qq.com>
2019-01-23 14:15:39 +0800

    连接两个通道(transferTo / transferFrom), 进行文件复制

---
+++ src/Chapter18_IO/TransferTo.java
@@ -0,0 +1,26 @@
+package Chapter18_IO;
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
+/**
+ *  连接两个通道(transferTo / transferFrom), 进行文件复制
+ */
+public class TransferTo {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("arguments : sourcefile destfile");
+            System.exit(1);
+        }
+        // 定义输入输出通道
+        FileChannel
+                in  = new FileInputStream(args[0]).getChannel(),
+                out = new FileOutputStream(args[1]).getChannel();
+
+        in.transferTo(0, in.size(), out);
+        // OR :
+        // out.transferFrom(in, 0, in.size());
+    }
+}
