a5f97393d572c564a19f273d61ae28fa99b19e38
zhouyf <376298333@qq.com>
2019-04-09 16:26:58 +0800

    使用 Condition 来重写 "车抛光和打蜡"

---
+++ src/Chapter20_Concurrency/waxomatic2/WaxOMatic2.java
@@ -0,0 +1,120 @@
+package Chapter20_Concurrency.waxomatic2;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *  使用 Condition 来重写 "车抛光和打蜡"
+ */
+
+class Car {
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+    private boolean waxOn = false;
+
+    public void waxed() {
+        lock.lock();
+        try {
+            waxOn = true;   // Ready to buff
+            condition.signalAll();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    public void buffed() {
+        lock.lock();
+        try {
+            waxOn = false;
+            condition.signalAll();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    public void waitForWaxing() throws InterruptedException {
+        lock.lock();
+        try {
+            while (waxOn == false)
+                condition.await();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    public void waitForBuffing() throws InterruptedException {
+        lock.lock();
+        try {
+            while (waxOn == true)
+                condition.await();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+}
+
+class WaxOn implements Runnable {
+    private Car car;
+    public WaxOn(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                System.out.println("Wax On!");
+                TimeUnit.MILLISECONDS.sleep(200);
+                car.waxed();
+                car.waitForBuffing();
+            }
+        }
+        catch (InterruptedException e) {
+            System.out.println("Exiting via interrupt");
+        }
+        System.out.println("Ending Wax On Task!");
+    }
+}
+
+class WaxOff implements Runnable {
+    private Car car;
+    public WaxOff(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                car.waitForWaxing();
+                System.out.println("    Wax Off!");
+                TimeUnit.MILLISECONDS.sleep(200);
+                car.buffed();
+            }
+        }
+        catch (InterruptedException e) {
+            System.out.println("Exiting via interrupt!");
+        }
+        System.out.println("Ending Wax Off Task!");
+    }
+}
+
+public class WaxOMatic2 {
+    public static void main(String[] args) throws Exception{
+        Car car = new Car();
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(new WaxOff(car));
+        exec.execute(new WaxOn(car));
+        TimeUnit.SECONDS.sleep(5);
+        exec.shutdownNow();
+    }
+}
